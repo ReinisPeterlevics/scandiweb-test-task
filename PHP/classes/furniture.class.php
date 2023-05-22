7b94756f999700500d2f3f0b666bc5fa38604834
Reinis Peterlevics <reinis.peterlecis@gmail.com>
2023-05-22 19:36:18 +0300

    Validation improvements

--- PHP/classes/furniture.class.php
+++ PHP/classes/furniture.class.php
@@ -32,7 +32,7 @@ class Furniture extends Product
       $errors["height"] = "Please, enter the height!";
       return $errors;
     }
-    if (strlen(substr(strrchr($height, "."), 1)) > 0 || $height < 0 || !is_numeric($height)) {
+    if (strlen(substr(strrchr($height, "."), 1)) > 0 || $height < 0 || !is_numeric($height) || strlen($height) > 10) {
       $errors["height"] = "Please, provide the height correctly!";
       return $errors;
     }
@@ -45,7 +45,7 @@ class Furniture extends Product
       $errors["width"] = "Please, enter the width!";
       return $errors;
     }
-    if (strlen(substr(strrchr($width, "."), 1)) > 0 || $width < 0 || !is_numeric($width)) {
+    if (strlen(substr(strrchr($width, "."), 1)) > 0 || $width < 0 || !is_numeric($width) || strlen($width) > 10) {
       $errors["width"] = "Please, provide the width correctly!";
       return $errors;
     }
@@ -58,7 +58,7 @@ class Furniture extends Product
       $errors["length"] = "Please, enter the length!";
       return $errors;
     }
-    if (strlen(substr(strrchr($length, "."), 1)) > 0 || $length < 0 || !is_numeric($length)) {
+    if (strlen(substr(strrchr($length, "."), 1)) > 0 || $length < 0 || !is_numeric($length) || strlen($length) > 10) {
       $errors["length"] = "Please, provide the length correctly!";
       return $errors;
     }
